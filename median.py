d3eee6548471a35589cc2a3e8ef32e244692cffa
AdityaPune <adityakho999@gmail.com>
2021-04-26 20:33:17 +0530

    And another one

---
+++ median.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        
+        n = len(nums1)//2
+        
+        if len(nums1)%2==0:
+            return float((nums1[n-1]+nums1[n])/2)
+        else:
+            return float(nums1[n])
